3eeb037bff3f7161665fad8960b81c7c96dafe3e
Ahn-hj <stella2003@naver.com>
2024-08-13 17:01:36 +0900

    2024-08-13

---
+++ test19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int i, a[5], temp;
+    for ( i = 0; i < 5; i++)
+    {
+        a[i] = i + 1;
+        printf("%d",a[i]);
+    }
+    printf("\n");
+    temp = a [0];
+    for (i = 0; i<4; i++)
+    {
+        a[i] = a[i+i];
+    }
+    a[4] = temp;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d",a[i]);
+    }
+    
+
+    
+}
--- test8.c
+++ test8.c
@@ -5,6 +5,6 @@ int main(void)
  float a = 123.456;
 
  i = (int)a;
- printf("%d, %3.2f", i, a);
+ printf("%d, %3.2f", i, a);//%3.2f 3은 출력할수있는공간이 세자리
  return 0;
 }
