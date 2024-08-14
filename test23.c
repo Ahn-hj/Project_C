8b3216ccdb309acea64b80afc485e4cb40641dfb
Ahn-hj <stella2003@naver.com>
2024-08-14 16:07:47 +0900

    2024-08-14

--- test23.c
+++ test23.c
@@ -8,15 +8,15 @@ int main() {
         2,3,4,5,5,
         5,2,4,1,1};
         int i = 0, j = 0;
-        int res = map[i][j];
+        int res = map[i][j]; //res = map[0][0] = 1
         while(1)
         {
-            if(i== 4 && j == 4)break;
-            else if (i == 4 )j++;
-            else if (j == 4 )i++;
-            else if (map[i+1][j] >= map[i][j+1])j++;
+            if(i== 4 && j == 4)break;// i랑 j가 4면 break
+            else if (i == 4 )j++; //i가 4면 j증가
+            else if (j == 4 )i++; //j가 4면 i증가
+            else if (map[i+1][j] >= map[i][j+1])j++; //map[i+1][j] 가 map[i][j+1]보다 같거나 크면 j증가
             else
-                i++;
+                i++; //아니면 i증가
             res += map[i][j]; 
         }
         printf("result : %d,res",res);
